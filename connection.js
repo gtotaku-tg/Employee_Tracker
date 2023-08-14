1c5fc5f492699cb25537defd0504a2b9526286bd
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-14 15:52:15 -0400

    minor fix

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+
 const mysql = require('mysql2');
 
 
 const db = mysql.createConnection({
     host: 'localhost',
-    // port: 3306,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
