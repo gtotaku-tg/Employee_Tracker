eb4890ed0bbd8c06f9e0061115dd374cc62e9fb0
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-14 15:17:23 -0400

    create connection js file

--- connection.js
+++ connection.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const db = mysql.createConnection(
     {
         host: 'localhost',
+        port: 3306,
         // Your MySQL username,
-        user: process.env.DB_USER,
+        user: "root",
         // Your MySQL password
-        password: process.env.DB_PW,
+        password: process.env.DB_PASSWORD,
         database: 'employeeTrackerDB'
     },
     console.log('Connected to the employeeTrackerDB database.')
